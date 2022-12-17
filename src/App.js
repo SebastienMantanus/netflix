111a697234c67462c8cc1f4f4cbe2de6b0621ffb
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2022-12-17 16:26:59 +0100

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Movies from "./movies_rnexgr.json";
+import Section from "./components/Section";
+import logo from "../src/logo_netflix.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img alt="logo" src={logo} />
       </header>
+      <Section />
     </div>
   );
 }
---
+++ src/components/Section.js
@@ -0,0 +1,7 @@
+// import Movies from "../movies_rnexgr.json";
+
+const Section = () => {
+  return <h1>TOTO</h1>;
+};
+
+export default Section;
