432cf8eaa2ed203a994945985c115d96928792b5
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2022-12-17 16:45:09 +0100

    immage array

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import "./App.css";
 import Movies from "./movies_rnexgr.json";
-import Section from "./components/Section";
+// import Section from "./components/Section";
 import logo from "../src/logo_netflix.png";
+import Section from "./components/Section";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <header>
         <img alt="logo" src={logo} />
       </header>
-      <Section />
+      {Movies.map((element, index) => {
+        // console.log(element.category);
+        return (
+          <Section
+            key={index}
+            category={element.category}
+            pictures={Movies.images.map((element, index) => {
+              console.log(element.images);
+            })}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,7 @@
 // import Movies from "../movies_rnexgr.json";
 
-const Section = () => {
-  return <h1>TOTO</h1>;
+const Section = (props) => {
+  return <h1>{props.category}</h1>;
 };
 
 export default Section;
